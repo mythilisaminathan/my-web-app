1b6b2128c3ad59bd0fcf17ce9945ca5500d60a92
mythilisaminathan <mythilisaminathan2501@gmail.com>
2025-01-04 19:39:47 +0530

    Fix: Re-add src/assets as a regular directory

---
+++ src/assets/mywork_data.js
@@ -0,0 +1,24 @@
+import project1_img from "../assets/movix.png";
+import project2_img from "../assets/crypto.png";
+
+const mywork_data = [
+  {
+    w_name: "Movix",
+    w_img: project1_img,
+    w_desc:
+      "Movix app is one the best movies database application made using TMDB API. Infinte scrolling, skeleton loading and filters made this project really awesome. And SCSS is used for designing of this website.",
+    w_tech: "React Redux SCSS API Infinite Scroll Filter Search",
+    githubLink: "https://github.com/mythilisaminathan/movix-by-mythili",
+    liveLink: "https://crypto-world-ssb.netlify.app/",
+  },
+  {
+    w_no: 2,
+    w_name: "Crypto Tracker",
+    w_img: project2_img,
+    w_desc:
+      "The app is designed to cater to cryptocurrency enthusiasts, traders, and investors who require quick access to market data and insights. With its user-friendly interface and advanced features, Crypto Tracker makes it easy to stay updated with the volatile crypto market.",
+    w_tech: "React Material UI Chart JS Context API Coin Gecko API",
+  }
+];
+
+export default mywork_data;
---
+++ src/assets/services_data.js
@@ -0,0 +1,80 @@
+const Services_Data = [
+    {
+        s_no:"01",
+        s_name:"Split Calculator",
+        s_desc:"Crafting innovative web solutions through code and creativity...",
+        s_tech:"HTML CSS JS",
+          githubLink:"https://github.com/mythilisaminathan/Split-Calculator",
+         livelink:"https://split-calci.netlify.app"
+    },
+    {
+        s_no:"02",
+        s_name:"Comedy Generator",
+        s_desc:"Developed with HTML, CSS, and JavaScript, this web app pulls jokes dynamically from an API, ensuring a constant supply of humor.",
+        s_tech:"HTML CSS JS DOM API",
+          githubLink:"https://github.com/mythilisaminathan/Comedy-Generator-web-API",
+         livelink:"https://mythilisaminathan.github.io/Comedy-Generator-web-API/"
+    },
+    {
+        s_no:"03",
+        s_name:"Currency Converter",
+        s_desc:"Seamless currency conversion using my React-powered tool. Switch b/w currencies real-time data from a reliable API.",
+        s_tech:"React Tailwind",
+          githubLink:"https://github.com/mythilisaminathan/currency-converter",
+         livelink:"https://currency-converter-rose-eight.vercel.app/"
+
+    },
+    {
+        s_no:"04",
+        s_name:"Scientific Calculator",
+        s_desc:"This calculator provides a lifelike feel, complete with a responsive design for various devices. Switch effortlessly between light and dark modes.",
+        s_tech:"HTML CSS JS DOM",
+          githubLink:"https://github.com/mythilisaminathan/scientific-calculator",
+         livelink:"https://mythilisaminathan.github.io/scientific-calculator/"
+
+    },
+    {
+        s_no:"05",
+        s_name:"Todo App",
+        s_desc:"Organize your tasks effortlessly with my ToDo app. Built with HTML, CSS, and JavaScript, this tool lets you add, delete, and mark tasks as done.",
+        s_tech:"HTML CSS JS DOM",
+          githubLink:"https://github.com/mythilisaminathan/Todo-app",
+         livelink:"https://todo-app-by-mythili.netlify.app/"
+
+    },
+    {
+        s_no:"06",
+        s_name:"Github Profile Finder",
+        s_desc:"Crafted using HTML, CSS, and JavaScript DOM, it fetches your GitHub data via API, presenting a clean showcase of repositories.",
+         s_tech:"HTML CSS JS DOM",
+           githubLink:"https://github.com/mythilisaminathan/github-profile-finder",
+         livelink:"https://mythilisaminathan.github.io/github-profile/"
+    },
+    {
+        s_no:"07",
+        s_name:"Notes App",
+        s_desc:"Crafted using HTML, CSS, and JavaScript DOM, it fetches your GitHub data via API, presenting a clean showcase of repositories.",
+         s_tech:"HTML CSS JS DOM",
+         githubLink:"https://github.com/mythilisaminathan/I2global-task",
+         livelink:"https://i2global-task.vercel.app/"
+
+    },
+    {
+        s_no:"08",
+        s_name:"Photography web",
+        s_desc:"Focused on delivering a seamless user experience, this project is a testament to modern web design principles.",
+         s_tech:"Figma & plugins",
+           githubLink:"",
+         livelink:"https://www.figma.com/design/2SQDhMTeY5OBCpPIdyFddu/photography?node-id=0-1&p=f&t=QDCWBXhgbvxn97Og-0"
+    },
+    {
+        s_no:"09",
+        s_name:"Landing Page",
+        s_desc:"Focused on delivering a seamless user experience, this project is a testament to modern web design principles.",
+         s_tech:"Figma & plugins",
+        githubLink:"",
+         livelink:"https://www.figma.com/design/SrRMXnrER2zI8Ff75OBApX/Gen-platform-landing-page?t=QDCWBXhgbvxn97Og-0"
+    },
+]
+ 
+export default Services_Data;
